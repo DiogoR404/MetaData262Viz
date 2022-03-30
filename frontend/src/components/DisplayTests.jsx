5df4326d9d38b9420f41702d930ab6e382c1f63b
Diogo Reis <ist187526@tecnico.ulisboa.pt>
2022-03-30 02:02:23 +0100

    attacted to previous commit

---
+++ frontend/src/components/DisplayTests.jsx
@@ -0,0 +1,33 @@
+import Stack from '@mui/material/Stack';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+
+const DisplaTests = ({ tests, presentType }) => {
+  const displayTest = (test, presentType) => {
+    if (presentType === 'Path') {
+      return test.path;
+    } else {
+      return (
+        <pre style={{ width: '100%', overflow: 'auto', fontFamily: 'inherit'}}>
+          {JSON.stringify(test, null, 4)}
+        </pre>
+      );
+    }
+  }
+  return (
+    <>
+      <Stack spacing={2}>
+        {tests.map((test) => {
+          return (
+            <Paper elevation={2} key={test._id}>
+              <Typography variant='subtitle1' content='p'>
+                {displayTest(test, presentType)}
+              </Typography>
+            </Paper>
+          )
+        })}
+      </Stack>
+    </>
+  );
+}
+export default DisplaTests
