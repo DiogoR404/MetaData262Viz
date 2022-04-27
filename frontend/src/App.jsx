6064a851ea4e7d37b8156723f7eddeb65d6e2e51
DiogoR404 <ist187526@tecnico.ulisboa.pt>
2022-04-27 12:21:50 +0100

    fix hardcoded backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css';
 import TestSearch from './components/TestSearch'
 
 function App() {
-  const url = 'http://localhost:5000/'
+  const url = window.location.href.slice(0,-5) + '5000/'
   return (
     <>
       <TestSearch url={url}/>
